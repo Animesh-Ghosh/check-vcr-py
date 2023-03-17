78aadd384093bf07ee460b5c29cc7cb21c2f8796
Animesh-Ghosh <ani101098@yahoo.in>
2023-03-18 01:39:48 +0530

    Add TOML serializer for people not too fond of YAML
    Updated test for json placeholder API to use the TOMLSerializer for
    serializing and deserializing vcr cassette files.
    Most importantly, removed YAML files.

--- test_json_placeholder_api.py
+++ test_json_placeholder_api.py
@@ -1,11 +1,31 @@
 import unittest
+import toml
 import vcr
 from json_placeholder_api import Post, get_posts, create_post
 
 
+class TOMLSerializer:
+    def serialize(self, cassette_dict):
+        return toml.dumps(cassette_dict)
+
+    def deserialize(self, cassette_string):
+        cassette_dict = toml.loads(cassette_string)
+        if 'body' not in cassette_dict['interactions'][0]['request']:
+            # TOML does NOT have a concept of null
+            cassette_dict['interactions'][0]['request']['body'] = None
+        if isinstance(cassette_dict['interactions'][0]['response']['body']['string'], list):
+            # TOML seems to store bytes object as a array
+            cassette_dict['interactions'][0]['response']['body']['string'] = bytes(cassette_dict['interactions'][0]['response']['body']['string'])
+        return cassette_dict
+
+
 class TestJSONPlaceholderAPI(unittest.TestCase):
+    def setUp(self):
+        self.vcr = vcr.VCR()
+        self.vcr.register_serializer('toml', TOMLSerializer())
+
     def test_get_posts(self):
-        with vcr.use_cassette('fixtures/cassettes/test_get_posts.yaml') as cassette:
+        with self.vcr.use_cassette('fixtures/cassettes/test_get_posts.toml', serializer='toml') as cassette:
             posts = get_posts()
             self.assertEqual('https://jsonplaceholder.typicode.com/posts', cassette.requests[0].uri)
             self.assertEqual('GET', cassette.requests[0].method)
@@ -15,7 +35,7 @@ class TestJSONPlaceholderAPI(unittest.TestCase):
             self.assertListEqual(['userId', 'id', 'title', 'body'], list(post.keys()))
 
     def test_create_post(self):
-        with vcr.use_cassette('fixtures/cassettes/test_create_post.yaml') as cassette:
+        with self.vcr.use_cassette('fixtures/cassettes/test_create_post.toml', serializer='toml') as cassette:
             post = Post(title='foo', body='bar', user_id=1)
             response = create_post(post)
             self.assertEqual('https://jsonplaceholder.typicode.com/posts', cassette.requests[0].uri)
